13f9a5a81da922ebb2138af4f9ee2a1d26de3e59
liujinli <123456@163.com>
2017-03-30 17:31:42 +0800

    redis基础版本

---
+++ third_party/Redis/Redis.h
@@ -0,0 +1,165 @@
+#ifndef __REDIS_H__
+#define __REDIS_H__
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+
+#include "HelpTemplate.h"
+#include "hiredis.h"
+
+
+#define REDIS_REPLY_TRUE "OK"
+
+
+
+class Redis
+{
+public:
+	Redis()	
+	{
+	}
+	~Redis()
+	{
+	}
+
+	/**
+	 * @brief 链接redis  
+	 *
+	 * @param host redis ip地址
+	 * @param port 端口号
+	 *
+	 * @return 
+	 */
+	bool Connect(const char* host,int port);
+
+	/**
+	 * @brief  获得链接失败原因
+	 *
+	 * @return 
+	 */
+	const char* GetConnectError()
+	{
+		return _connect->errstr;	
+	}
+
+	/**
+	 * @brief 获取的值
+	 *
+	 * @param key
+	 *
+	 * @return 
+	 */
+	std::string Get(const char* key);
+
+	/**
+	 * @brief 设置值
+	 *
+	 * @param key
+	 * @param value
+	 *
+	 * @return 
+	 */
+	bool Set(const char* key,const char* value);
+
+
+	/**
+	 * @brief 删除key
+	 *
+	 * @param key
+	 *
+	 * @return 
+	 */
+	template <typename ...T>
+	int Del(T ... keyList)
+	{
+
+		CheckString(keyList...);
+		std::string cmd = "del "+ GetFormattingType(keyList...);
+		int delNum = 0;
+		_reply= Command(cmd.c_str(),keyList...); 
+		if(_reply != nullptr)
+		{    
+			delNum = _reply->integer;
+		}   
+	
+		return delNum;
+	};
+
+
+
+
+	template <typename T,typename ...ValueList>
+	void CheckString(T v,ValueList ...list){
+		//这里要判断所有参数是不是字符串
+		static_assert(CheckType<const char*,T>::value,"error parameter is not string");
+		CheckString(list...);
+	}
+
+	template<typename T>
+	void CheckString(T v)
+	{
+		static_assert(CheckType<const char*,T>::value,"error parameter is not string");
+	}
+
+
+
+	template<typename ...T>
+	redisReply*Command(const char* cmd,T ... value)
+	{
+		FreeReply(_reply);
+		_reply=static_cast<redisReply*>(redisCommand(_connect,cmd,value ...));
+		return _reply;
+	}
+	
+	template<typename Type ,typename... TypeList>
+    std::string GetFormattingType(Type value,TypeList... typeList)
+	{
+		return  GetFormattingType(value) +" "+ GetFormattingType(typeList ...);
+	};
+
+
+	std::string GetFormattingType(float)
+	{
+		return "%f";	
+	}
+
+	std::string GetFormattingType(int)
+	{
+		return "%d";	
+	}
+
+	 std::string GetFormattingType(const char*)
+	{
+		return "%s";	
+	}
+
+
+	template<class T>
+	std::string GetFormattingType(T value)
+	{
+		static_assert(error<T>::value,"GetFormattingType unknown parameter type !");
+		return "";
+	}
+
+private:
+	void FreeReply(redisReply*reply)
+	{
+		if(reply != nullptr)
+		{
+			freeReplyObject(reply);
+		}
+	
+	};
+
+
+
+	private:
+		redisContext * _connect{nullptr};
+	    redisReply* _reply{nullptr};
+	     
+};
+
+
+#endif
