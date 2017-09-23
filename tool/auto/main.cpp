a2067eb36de150e87aaa3afa9dd8cc15a77069c6
liujinli <123456@163.com>
2017-09-23 15:16:54 +0800

    map push

--- tool/auto/main.cpp
+++ tool/auto/main.cpp
@@ -9,81 +9,121 @@ typedef char Yes;
 typedef Yes No[2];
 
 
-template<typename T>
-struct HashFunc{
-    template<typename U>
-    static Yes& Check(decltype(U::GetSecondKey));
-    template<typename U>
-    static  No &Check(...);
-    static const constexpr int value = sizeof(Check<T>(0)) == sizeof(Yes);
+#define DEFINE_CHECK_FUNC(X) \
+    template<typename T>  \
+struct Check##X{  \
+    template<typename U> \
+    static Yes& Check(decltype(&U::X));\
+    template<typename U>   \
+    static No &Check(...);   \
+    static const constexpr int value = (sizeof(Check<T>(0))==sizeof(Yes)); \
 };
 
-
-
+DEFINE_CHECK_FUNC(Hash3Key);
+DEFINE_CHECK_FUNC(Hash2Key);
+DEFINE_CHECK_FUNC(Hash1Key);
 
 template<class T,class M>
 struct smap:public std::map<T,M>
 {
-    template<class Z>
-    void Push(Z z){
-        cout<<"value="<<HashFunc<Z>::value<<endl;
-        if(HashFunc<Z>::value)
-        {
-            //this->insert(std::make_pair(m.GetFirstKey(),m));
-            //this->insert(make_pair(m.GetSecondKey(), Push(m)));
-               //this->insert(make_pair(z.GetFirstKey(),make_pair(z.GetSecondKey(),z)));
-               cout<<"push"<<endl;
-               (*this)[z.GetFirstKey()][z.GetSecondKey()] =z;
-               //pp(this,z);
+   template<typename U>
+   typename enable_if<CheckHash3Key<U>::value,void>::type Push(U u){
+           (*this)[u.GetFirstKey()][u.GetSecondKey()][u.GetThreeKey()] =u;
+   };
 
-        }
-        else{
-//            this->insert(std::make_pair(z.GetFirstKey(),z));
-        }
-    }
     template<typename U>
-    void pp(std::map<T,M>* m,U& u){
-        (*m)[u.GetFirstKey()][u.GetSecondKey()] = u;
-    }
- //   template<typename X,typename Y>
- //   void Push(std::map<X,Y>&m){
- //       this->insert(std::make_pair(m.seocnd.GetFirstKey(),m));
- //   }
+    typename enable_if<CheckHash2Key<U>::value,void>::type Push(U u){
+            (*this)[u.GetFirstKey()][u.GetSecondKey()] =u;
+    };
 
-
-
-
+    template<typename U>
+     typename enable_if<CheckHash1Key<U>::value,void>::type Push(U u){
+            this->insert(std::make_pair(u.GetFirstKey(),u));
+    };
 
 };
 
 struct Friend{
+    void Hash2Key();
     std::string GetFirstKey(){return name;}
     int GetSecondKey(){return age;}
     std::string  name;
     int age;
-    int id;
+    int id{111};
 
+};
+
+struct User{
+
+    void Hash1Key();
+    std::string GetFirstKey(){return userName;}
+    std::string userName;
 };
 
 
 
+struct Npc{
+    
+    void Hash3Key();
+    std::string GetFirstKey(){return name;};
+    int GetSecondKey(){return id;};
+    int GetThreeKey(){return att;};
+
+    std::string name;
+    int id;
+    int att;
+
+
+
+};
+
+#include<utility>
+
 int main()
 {
+    typedef smap<std::string,smap<int,smap<int,Npc>>>  NPC;
+    NPC npcList;
+    Npc npc;
+    npc.name="npc";
 
-    smap<std::string ,Friend> friendList;
+    
+
+
+   // cout<<"xxx="<<HashFunc1<Npc>::value<<endl;
+   // cout<<"xxx="<<HashFunc2<Npc>::value<<endl;
+   // cout<<"xxx="<<HashFunc3<Npc>::value<<endl;
+    npcList.Push(npc); 
+    for(auto it:npcList)
+    {
+       cout<<"npc name"  <<it.second.begin()->second.begin()->second.name.c_str()<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    smap<std::string ,User> userList;
     smap<std::string ,smap<int,Friend> > f1;
 
     Friend f;
     f.name = "a";
     f.age = 10;
 
-    //friendList.Push(f);
+    User user;
+    user.userName = "abc";
+    userList.Push(user);
 
     f1.Push(f);
 
-    for(auto it:friendList)
+    for(auto it:userList)
     {
-        cout<<"name="<<it.second.name.c_str()<<"  age="<<it.second.age<<endl;
+        cout<<"name="<<it.second.userName.c_str()<<endl;
     
     }
 
@@ -91,10 +131,10 @@ int main()
     {
         //cout<<"name="<<it.second.name.c_str()<<"  age="<<it.second.age<<endl;
         //
-        cout<<"firstkey = "<<it.first;
+        cout<<"firstkey = "<<it.first.c_str()<<endl;;
 
         for(auto its:it.second){
-            cout<<"second="<<its.first<<"id="<<its.second.id;
+            cout<<"second="<<its.first<<"\n"<<"id="<<its.second.id;
         }
     
     }
