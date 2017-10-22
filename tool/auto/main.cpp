7e50f1a8dda06b135a1f9944665588b8c0be70d7
supersauli <sauli.good@163.com>
2017-10-22 22:07:30 +0800

    del

--- tool/auto/main.cpp
+++ tool/auto/main.cpp
@@ -19,11 +19,11 @@ int main(){
         userMap.Push(user);
     }
     cout<<ContainersDepth<decltype(userMap)>()<<endl;
-    cout<<CheckContainersDepth<decltype(userMap),0>::value<<endl;
-    cout<<CheckContainersDepth<decltype(userMap),1>::value<<endl;
-    cout<<CheckContainersDepth<decltype(userMap),2>::value<<endl;
-    cout<<CheckContainersDepth<decltype(userMap),3>::value<<endl;
-
+//    cout<<CheckContainersDepth<decltype(userMap),0>::value<<endl;
+//    cout<<CheckContainersDepth<decltype(userMap),1>::value<<endl;
+//    cout<<CheckContainersDepth<decltype(userMap),2>::value<<endl;
+//    cout<<CheckContainersDepth<decltype(userMap),3>::value<<endl;
+//
     smap<string,map<int,map<int,Npc>>> npcMap;
     Npc npc;
     npc.name = "11111";
@@ -35,7 +35,7 @@ int main(){
         npcMap.Del(it);
         if(!npcMap.empty())
         {
-            printf("error Del \n");
+            printf(" x1 error Del \n");
         }
 
         npcMap.Push(npc);
@@ -45,92 +45,92 @@ int main(){
             auto its = it->second.begin();
             for(;its!= it->second.end();its++)
             {
-                //npcMap.Del(its); 
+                npcMap.Del(its); 
             }
         }
 
 
-     if(!npcMap.empty())
-        {
-            printf("error Del \n");
-        }
+		if(!npcMap.empty())
+		{
+			printf("x2 error Del \n");
+		}
 
 
- if(!npcMap.empty())
-        {
-            printf("error Del \n");
-        }
-
-
-
-
-    }
+		if(!npcMap.empty())
+		{
+			printf("error Del \n");
+		}
 
 
 
+
+	}
 
 
 
 
 
-    std::map<string,std::vector<User>>::iterator it = userMap.begin(); 
-    for(;it!=userMap.end();it++)
-    {
-        //userMap.Del(it);
-        cout<<"itvalue0 = "<<CheckIt<0,decltype(userMap),decltype(it)>::value<<endl;
-        cout<<"itvalue1 = "<<CheckIt<1,decltype(userMap),decltype(it)>::value<<endl;
-        cout<<"itvalue2 = "<<CheckIt<2,decltype(userMap),decltype(it)>::value<<endl;
-        break;
-        //userMap.erase(it);
-    }
 
 
 
-
-    cout<<"#########################################################"<<endl;
-
-      it = userMap.begin(); 
-
-
-
-    for(;it!=userMap.end();it++)
-    {
-        auto its = it->second.begin();
-        //decltype(it)::value_type f;
-         //cout<<"x1="<<Checkbegin<decltype(it)>::value<<endl;
-
-         //cout<<"  "<<typeid(f).name()<<endl;
-         //cout<<" b1 "<<CheckHash1Key<typename decltype(it)::value_type>::value <<endl;
-        for(;its!=it->second.end();its++)
-        {
-        cout<<"itvalue0 = "<<CheckIt<0,decltype(userMap),decltype(its)>::value<<endl;
-        cout<<"itvalue1 = "<<CheckIt<1,decltype(userMap),decltype(its)>::value<<endl;
-        cout<<"itvalue2 = "<<CheckIt<2,decltype(userMap),decltype(its)>::value<<endl;
-         userMap.Del(its);
-         //cout<<"x2="<<Checkbegin<decltype(its)>::value<<endl;
-        decltype(its)::value_type f1;
-        it->second.erase(its);
-
-         cout<<"  "<<typeid(f1).name()<<endl;
-         cout<<" b2 "<<CheckHash1Key<typename decltype(its)::value_type>::value <<endl;
-         break;
-
-        }
-        break;
-    }
-
-
-
-
-
-    cout<<"del"<<endl;
-    for(auto it:userMap )
-    {
-        for(auto its:it.second) {
-            cout<<its.userName.c_str()<<endl;
-        }
-    }
-
+//    std::map<string,std::vector<User>>::iterator it = userMap.begin(); 
+//    for(;it!=userMap.end();it++)
+//    {
+//        //userMap.Del(it);
+//        cout<<"itvalue0 = "<<CheckIt<0,decltype(userMap),decltype(it)>::value<<endl;
+//        cout<<"itvalue1 = "<<CheckIt<1,decltype(userMap),decltype(it)>::value<<endl;
+//        cout<<"itvalue2 = "<<CheckIt<2,decltype(userMap),decltype(it)>::value<<endl;
+//        break;
+//        //userMap.erase(it);
+//    }
+//
+//
+//
+//
+//    cout<<"#########################################################"<<endl;
+//
+//      it = userMap.begin(); 
+//
+//
+//
+//    for(;it!=userMap.end();it++)
+//    {
+//        auto its = it->second.begin();
+//        //decltype(it)::value_type f;
+//         //cout<<"x1="<<Checkbegin<decltype(it)>::value<<endl;
+//
+//         //cout<<"  "<<typeid(f).name()<<endl;
+//         //cout<<" b1 "<<CheckHash1Key<typename decltype(it)::value_type>::value <<endl;
+//        for(;its!=it->second.end();its++)
+//        {
+//        cout<<"itvalue0 = "<<CheckIt<0,decltype(userMap),decltype(its)>::value<<endl;
+//        cout<<"itvalue1 = "<<CheckIt<1,decltype(userMap),decltype(its)>::value<<endl;
+//        cout<<"itvalue2 = "<<CheckIt<2,decltype(userMap),decltype(its)>::value<<endl;
+//         userMap.Del(its);
+//         //cout<<"x2="<<Checkbegin<decltype(its)>::value<<endl;
+//        //decltype(its)::value_type f1;
+//        //it->second.erase(its);
+//
+//         //cout<<"  "<<typeid(f1).name()<<endl;
+//         cout<<" b2 "<<CheckHash1Key<typename decltype(its)::value_type>::value <<endl;
+//         break;
+//
+//        }
+//        break;
+//    }
+//
+//
+//
+//
+//
+//    cout<<"del"<<endl;
+//    for(auto it:userMap )
+//    {
+//        for(auto its:it.second) {
+//            cout<<its.userName.c_str()<<endl;
+//        }
+//    }
+//
 
 
 
